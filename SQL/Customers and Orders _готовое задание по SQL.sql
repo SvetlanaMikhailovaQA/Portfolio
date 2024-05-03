749e47eb56b5d0c2a21de7a9d3e81a9e45dbf5f8
mih sve <RMS4083@yandex.ru>
2024-05-03 11:23:51 +0300

    Обновлено решение задания по SQL

--- SQL/Customers and Orders _готовое задание по SQL.sql
+++ SQL/Customers and Orders _готовое задание по SQL.sql
@@ -18,20 +18,20 @@ OrderSum
 Вывести названия всех компаний, которые не выполнили заказ с 15.11.1996 – 18.02.1997 
 SELECT  CompanyName 
 FROM  Customers 
-lEFT JOIN Orders ON  Customers.CustomerID = Orders. CustomerID WHERE  OrderDate  NOT BETWEEN “15.11.1996” AND “18.02.1997”  
+lEFT JOIN Orders ON  Customers.CustomerID = Orders. CustomerID WHERE  OrderDate  NOT BETWEEN '15.11.1996' AND '18.02.1997' 
 
 Вывести названия компаний поставщиков, которые находятся в México D.F.;
-SELECT CompanyName FROM Customers WHERE City = ‘México D.F.’
+SELECT CompanyName FROM Customers WHERE City = 'México D.F.'
 
 Вывести только те заказы, которые были оформлены с 12.12.1996 по 18.02.1997;
-SELECT OrderID FROM Orders WHERE OrderDate BETWEEN ‘12.12.1996’ AND ‘18.02.1997’
+SELECT OrderID FROM Orders WHERE OrderDate BETWEEN '12.12.1996' AND '18.02.1997'
 
-Вывести только тех заказчиков,  название компании которых начинаются с ‘An’;
-SELECT CompanyName FROM Customers WHERE CompanyName LIKE  ‘An%’
+Вывести только тех заказчиков,  название компании которых начинаются с 'An';
+SELECT CompanyName FROM Customers WHERE CompanyName LIKE  'An%'
 
 Вывести названия компаний и заказы, которые они оформили, с суммой заказа более 17000. Использовать соединение таблиц. 
 SELECT CompanyName, OrderID 
 FROM Customers 
 INNER JOIN  Orders ON Customers.CustomerID = Orders. CustomerID
-WHERE  Orders.OrderSum>17000
+WHERE  Orders.OrderSum > 17000
 
